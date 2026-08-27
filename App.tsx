1c0c9091c10ef2dfa6b8597f9873bf835e89230f
juliaresplandes-web <julia.resplandes@aluno.senai.br>
2026-08-27 15:09:28 -0300

    feat: initialize project

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import { Ionicons } from '@expo/vector-icons';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <ScrollView style={styles.container}>
+      {/*Header */}
+      <View style={styles.header}>
+        <View>
+          <Text>Café do Código</Text>
+          <Text>Seu café, uma linha por vez</Text>
+        </View>
+        <View>
+          <Ionicons name='person' size={20} color="#2f2d2c"></Ionicons>
+        </View>
+      </View>
+      {/*Header */}
+
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#f9f9f9'
+  },
+  header:{
+    width:'100%',
+    paddingTop:60,
+    paddingHorizontal:24,
+    paddingBottom:20,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  }
+})
